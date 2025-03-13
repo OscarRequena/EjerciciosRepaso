be39dcf114a90a055a7c79a34872baae058de5f2
OscarRequena <oscarjair_7@hotmail.com>
2025-03-13 19:23:07 +0100

    conteo de digitos

---
+++ src/condicionalesYbucles/conteoDigito/conteoDigito.java
@@ -0,0 +1,18 @@
+package condicionalesYbucles.conteoDigito;
+
+import java.util.Scanner;
+
+public class conteoDigito {
+    public static void main(String[] args) {
+        System.out.println("Ingrese ");
+        Scanner scanner = new Scanner(System.in);
+        int variable = scanner.nextInt();
+        int cont = 0;
+        while(variable > 0){
+            variable /=10;
+            cont++;
+        }
+        System.out.println(cont);
+
+    }
+}
