1c857d854477bab3b1a3b7d7f9c9a807256edfd9
OscarRequena <oscarjair_7@hotmail.com>
2025-03-13 15:24:46 +0100

    el area del rectangulo

--- src/ConceptosBasicos/areaRectangulo/AreaRectangulo.java
+++ src/ConceptosBasicos/areaRectangulo/AreaRectangulo.java
@@ -2,5 +2,14 @@ package ConceptosBasicos.areaRectangulo;
 
 public class AreaRectangulo {
 
+    public static void main(String[] args) {
+       int largo = 12;
+       int ancho = 2;
+       areaRectangulo(largo,ancho);
+    }
 
+    public static void areaRectangulo(int largo, int ancho){
+        int area = largo * ancho ;
+        System.out.println("el area del rectangulo es: " + area);
+    }
 }
