1e095eedb96f90d83ba52b3c1adfdfa1aa596962
OscarRequena <oscarjair_7@hotmail.com>
2025-03-13 15:33:04 +0100

    convertir grados c a f

--- src/ConceptosBasicos/convertirGrados/ConvertirGrados.java
+++ src/ConceptosBasicos/convertirGrados/ConvertirGrados.java
@@ -1,4 +1,18 @@
 package ConceptosBasicos.convertirGrados;
 
 public class ConvertirGrados {
+
+    public static void main(String[] args) {
+        float celcius = 2;
+
+        convertirGrados(celcius);
+
+    }
+
+    public static void convertirGrados(float celcius){
+
+        float resultado = (celcius * 9/5) + 32;
+
+        System.out.println("el resultado de la operación es: " + resultado);
+    }
 }
